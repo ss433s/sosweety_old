67daa74a771632f4dac404bea07c3260329b2ac3
guoyu <guoyu@KS_BZD21629.kingsoft.cn>
2020-01-14 18:59:29 +0800

    e1

---
+++ experiment/e1.py
@@ -0,0 +1,41 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"Point({self.x},{self.y})"
+
+    def show(self):
+        return self
+
+    __repr__ = __str__
+
+
+p1 = Point(4, 5)
+p2 = Point(3, 2)
+
+print(repr(p1), repr(p2), sep='\n')
+print(p1.__dict__)
+setattr(p1, 'x', 5)
+setattr(p1, 'u', 5)
+print(getattr(p1, '__dict__'))
+
+if hasattr(p1, 'x'):
+    print('Yes, i have')
+    print(getattr(p1, 'xx', 2000))
+
+if not hasattr(Point, 'add'):
+    setattr(Point, 'add', lambda self,other: Point(self.x + other.x, self.y + other.y))
+
+print(Point.add)
+print(p1.add)
+print(p1.add(p2))
+
+if not hasattr(p1, 'sub'):
+    setattr(p1, 'sub', lambda self,other: Point(self.x - other.x, self.y - other.y))
+
+print(p1.sub)
+print(p1.sub(p1, p2))
+print(p1.__dict__)
+print(Point.__dict__)
